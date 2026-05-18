6a3a8e72cac1d7066c3a6a928ef8fecee9237e4c
shironeko <shironeko1052@gmail.com>
2026-05-17 23:33:44 -0700

    fix domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import {resolve} from 'path';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+import {cloudflare} from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss()],
   base: process.env.GITHUB_PAGES ? '/lets_play_some_chords/' : '/',
-=======
   plugins: [react(), tailwindcss(), cloudflare()],
-  base: '/lets_play_some_chords/', // GitHub Pages repo name
->>>>>>> 978abb40311a69a109040ea6eaf6efdbdd5a420f
   build: {
     rollupOptions: {
       input: {
